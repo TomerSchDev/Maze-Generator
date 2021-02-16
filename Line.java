7c91b0a821b0901604bf9ea21e53f0eabc186e09
Tomer Schwartz <40770296+TomerSchDev@users.noreply.github.com>
2021-02-16 21:22:02 +0200

    Add files via upload

--- Line.java
+++ Line.java
@@ -1,37 +1,54 @@
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
 
+/**
+ * The type Line.
+ */
 public class Line {
+    /**
+     * The X 1.
+     */
     private int x1;
+    /**
+     * The X 2.
+     */
     private int x2;
+    /**
+     * The Y 1.
+     */
     private int y1;
+    /**
+     * The Y 2.
+     */
     private int y2;
+    /**
+     * The Color of the line.
+     */
     private Color color;
 
-    public Line(int x1, int y1, int x2, int y2,Color color) {
+    /**
+     * Instantiates a new Line.
+     *
+     * @param x1    the x 1
+     * @param y1    the y 1
+     * @param x2    the x 2
+     * @param y2    the y 2
+     * @param color the color
+     */
+    public Line(int x1, int y1, int x2, int y2, Color color) {
         this.x1 = x1;
         this.x2 = x2;
         this.y1 = y1;
         this.y2 = y2;
-        this.color=color;
+        this.color = color;
     }
 
-    public int getX1() {
-        return this.x1;
-    }
-
-    public int getX2() {
-        return this.x2;
-    }
-
-    public int getY1() {
-        return this.y1;
-    }
-
-    public int getY2() {
-        return this.y2;
-    }
-
-    public void drawOn(Graphics g) {
+    /**
+     * Draw line.
+     *
+     * @param g the graphics that draw the line
+     */
+    public void drawLine(Graphics g) {
         g.setColor(color);
         g.drawLine(this.x1, this.y1, this.x2, this.y2);
     }
